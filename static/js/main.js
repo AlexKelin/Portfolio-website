a3270db017c2bcbc4070f301c601ac5624069640
alex <9272107@gmail.com>
2023-06-03 17:43:13 -0400

    Added Worksheets

--- static/js/main.js
+++ static/js/main.js
@@ -1,29 +1,34 @@
-
 (function($) {
 
 	var	$window = $(window),
 		$body = $('body'),
-		$nav = $('#nav');
+		$nav = $('#nav'),
+		$menuToggle = $('#menu-toggle');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+	breakpoints({
+		xlarge:  [ '1281px',  '1680px' ],
+		large:   [ '981px',   '1280px' ],
+		medium:  [ '737px',   '980px'  ],
+		small:   [ null,      '736px'  ]
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function() {
+		window.setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
+	$('#nav a, .scrolly').scrolly({
+		speed: 1000,
+		offset: function() { return $nav.height(); }
+	});
+
+	// Menu toggle.
+	$menuToggle.on('change', function() {
+		$nav.toggleClass('open');
+	});
 
 })(jQuery);
