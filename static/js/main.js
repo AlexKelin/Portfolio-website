31634b9562247a83e152448c74d50b564ad2a762
alex <9272107@gmail.com>
2023-06-04 11:49:48 -0400

    Added Worksheets

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,36 @@
 (function($) {
+  var $window = $(window),
+    $body = $('body'),
+    $nav = $('#nav'),
+    $menuToggle = $('#menu-toggle');
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav'),
-		$menuToggle = $('#menu-toggle');
+  // Breakpoints.
+  breakpoints({
+    xlarge: ['1281px', '1680px'],
+    large: ['981px', '1280px'],
+    medium: ['737px', '980px'],
+    small: [null, '736px'],
+  });
 
-	// Breakpoints.
-	breakpoints({
-		xlarge:  [ '1281px',  '1680px' ],
-		large:   [ '981px',   '1280px' ],
-		medium:  [ '737px',   '980px'  ],
-		small:   [ null,      '736px'  ]
-	});
+  // Play initial animations on page load.
+  $window.on('load', function() {
+    window.setTimeout(function() {
+      $body.removeClass('is-preload');
+    }, 100);
+  });
 
-	// Play initial animations on page load.
-	$window.on('load', function() {
-		window.setTimeout(function() {
-			$body.removeClass('is-preload');
-		}, 100);
-	});
+  // Scrolly.
+  $('#nav a, .scrolly').scrolly({
+    speed: 1000,
+    offset: function() {
+      return $nav.height();
+    },
+  });
 
-	// Scrolly.
-	$('#nav a, .scrolly').scrolly({
-		speed: 1000,
-		offset: function() { return $nav.height(); }
-	});
-
-	// Menu toggle.
-	$menuToggle.on('change', function() {
-		$nav.toggleClass('open');
-	});
-
+  // Menu toggle.
+  $menuToggle.on('change', function() {
+    $nav.toggleClass('open');
+  });
 })(jQuery);
+
+
